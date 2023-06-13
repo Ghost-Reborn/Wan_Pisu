d4dbcd7f0d34a2478908ef1c8d31b03b69c8ea08
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2023-06-13 13:18:08 +0530

    Check AllAnime API

--- app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
+++ app/src/main/java/in/ghostreborn/wanpisu/MainActivity.java
@@ -2,13 +2,69 @@ package in.ghostreborn.wanpisu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    static TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testText = findViewById(R.id.test_text);
+        AllAnimeAsync animeAsync = new AllAnimeAsync();
+        animeAsync.execute();
+
+    }
+}
+
+class AllAnimeAsync extends AsyncTask<Void, Void, String> {
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        return getJSONData();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        MainActivity.testText.setText(s);
+    }
+
+    private String getJSONData(){
+        try{
+            URL url = new URL(
+                    WanPisuConstants.ALL_ANIME_QUERY_HEAD +
+                            WanPisuConstants.ALL_ANIME_QUERY_TAIL
+            );
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            return response.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "ERROR";
+
     }
 }
---
+++ app/src/main/java/in/ghostreborn/wanpisu/WanPisuConstants.java
@@ -0,0 +1,8 @@
+package in.ghostreborn.wanpisu;
+
+public class WanPisuConstants {
+
+    public static final String ALL_ANIME_QUERY_HEAD = "https://api.allanime.to/allanimeapi?variables={%22search%22:{%22allowAdult%22:false,%22allowUnknown%22:false,%22query%22:%22";
+    public static final String ALL_ANIME_QUERY_TAIL = "%22},%22limit%22:40,%22page%22:1,%22translationType%22:%22sub%22,%22countryOrigin%22:%22ALL%22}&query=query($search:SearchInput,$limit:Int,$page:Int,$translationType:VaildTranslationTypeEnumType,$countryOrigin:VaildCountryOriginEnumType){shows(search:$search,limit:$limit,page:$page,translationType:$translationType,countryOrigin:$countryOrigin){edges{_id,name,availableEpisodes,__typename,malId}}}";
+
+}
